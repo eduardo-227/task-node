f3dd8700f646913518032ae299bbdc0c425949b2
Francisco Eduardo <134714192+Bodim-227@users.noreply.github.com>
2023-06-26 11:02:01 -0300

    put

--- backend/src/routes/TaskRoutes.js
+++ backend/src/routes/TaskRoutes.js
@@ -4,8 +4,9 @@ const router = express.Router();
 const TaskController = require('../controller/TaskController');
 
 
-router.post('/', TaskController.create);
+router.post('/',TaskValidation, TaskController.create);
 
+router.put('/:id',TaskValidation, TaskController.updated);
 
 
 module.exports = router;
